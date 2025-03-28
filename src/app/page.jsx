c9d7e2d92888b4061e1f2a354a863cefa0470423
mujeebbinazhar <shamimujeeb623@gmail.com>
2025-03-28 16:24:29 +0500

    under dev

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="dummy">
+        <img
+          src="/assets/img/under-dev.jpeg"
+          alt="limited marketing"
+          className="img-fluid"
+        />
+      </div>
+    </>
+  );
+};
+
+export default page;
